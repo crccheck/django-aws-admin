2f5bf6034c08c6754c25d14358a5ccefbeb9a0c7
crccheck <c@crccheck.com>
2015-08-01 22:37:55 -0500

    save a bunch of queries by just specifying the vpc pk

--- aws_admin/utils.py
+++ aws_admin/utils.py
@@ -87,14 +87,12 @@ def pull_security_groups(region=None):
     sg_cache = {}
     rs = conn.get_all_security_groups()
     for group in rs:
-        # FIXME just use vpc_id
-        vpc, __ = VPC.objects.get_or_create(id=group.vpc_id, defaults={'region': region})
         defaults = {
             'description': group.description,
             'name': group.name,
             'region': region,
             'tags': group.tags,
-            'vpc': vpc,
+            'vpc_id': group.vpc_id,
         }
         security_group, __ = obj_update_or_create(
             SecurityGroup,
